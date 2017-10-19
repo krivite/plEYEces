ae2ed806d8e83fbeaee8884e99414fdb061fedfd
Igor Rinkovec <shiqqqi@gmail.com>
2017-10-19 21:33:56 +0200

    Added a test AR ViewController.

---
+++ iOS/Pleyeces/Common/Models/PointOfInterest.swift
@@ -0,0 +1,19 @@
+//
+//  PointOfInterest.swift
+//  Pleyeces
+//
+//  Created by Igor Rinkovec on 19/10/2017.
+//  Copyright © 2017 Kristiana. All rights reserved.
+//
+
+import Foundation
+
+class PointOfInterest {
+    var name: String
+    var description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
