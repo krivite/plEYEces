f11b28b6d65fcb16d93a08197c6f93752ae6ac26
krivite <kristiana735@gmail.com>
2017-11-01 22:46:19 +0100

    Updated POI Detail List View.

--- iOS/Pleyeces/POIDetailViewController.swift
+++ iOS/Pleyeces/POIDetailViewController.swift
@@ -7,29 +7,51 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class POIDetailViewController: UIViewController {
 
+    var dict = NSDictionary()
+    
+    @IBOutlet weak var poiName: UILabel!
+    @IBOutlet weak var poiImage: UIImageView!
+    @IBOutlet weak var poiAddress: UILabel!
+    @IBOutlet weak var poiHours: UILabel!
+    @IBOutlet weak var poiInfo: UITextView!
+    @IBOutlet weak var poiOfferInfo: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        let urlString = "http://pleyec.es/api/poi/1"
+        
+        if let url = NSURL(string: urlString) {
+            
+            if let data = try? NSData(contentsOf: url as URL, options: []) {
+                
+                let json = JSON(data: data as Data)
+                
+                print(json)
+        
+                let name = json["name"].stringValue
+                let address = json["address"].stringValue
+                let workingHours = json["working_hours"].stringValue
+//                let poiInfo = json["details"].stringValue
+//                let offerInfo = json["offers"].stringValue
+                
+                poiName.text = name
+                poiAddress.text = address
+                poiHours.text = workingHours
+                
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
