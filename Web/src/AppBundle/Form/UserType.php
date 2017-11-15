07c8646942d4e0dabd55ca51639ad043c56d0fbc
monster811 <i.solic811@gmail.com>
2017-11-15 12:15:21 +0100

    Fixed user crud for admins, now it shows all fields from table

--- Web/src/AppBundle/Form/UserType.php
+++ Web/src/AppBundle/Form/UserType.php
@@ -8,7 +8,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
-
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('usernameCanonical')->add('email')->add('emailCanonical')->add('enabled')->add('salt')->add('password')->add('lastLogin')->add('confirmationToken')->add('passwordRequestedAt')->add('roles');
+    }
+    
     /**
      * {@inheritdoc}
      */
