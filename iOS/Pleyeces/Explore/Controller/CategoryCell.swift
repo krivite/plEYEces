5b0b778d1794005933a39fe62bfe2db54d4bc92e
monster811 <i.solic811@gmail.com>
2018-01-18 18:46:08 +0100

    Added explore view in main storyboard.

---
+++ iOS/Pleyeces/Explore/Controller/CategoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryCell.swift
+//  Pleyeces
+//
+//  Created by FOI on 18/01/2018.
+//  Copyright © 2018 Kristiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategoryCell: UICollectionViewCell{
+    
+}
---
+++ iOS/Pleyeces/Explore/Controller/ExplorePageController.swift
@@ -0,0 +1,35 @@
+//
+//  ExplorePageController.swift
+//  Pleyeces
+//
+//  Created by FOI on 18/01/2018.
+//  Copyright © 2018 Kristiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExplorePageController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
