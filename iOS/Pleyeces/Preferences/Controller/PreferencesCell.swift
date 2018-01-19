c2f20154aaefa2df81c14ba25f8ce64f36e26faa
krivite <kristiana735@gmail.com>
2018-01-19 20:28:41 +0100

    Modified Preferences screen, added buttons for poi categories

---
+++ iOS/Pleyeces/Preferences/Controller/PreferencesCell.swift
@@ -0,0 +1,16 @@
+//
+//  PreferencesCell.swift
+//  Pleyeces
+//
+//  Created by Kristiana on 19/01/2018.
+//  Copyright © 2018 Kristiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PreferencesCell: UICollectionViewCell {
+    
+    @IBOutlet weak var poiNameLbl: UILabel!
+    @IBOutlet weak var poiBtn: UIButton!
+}
---
+++ iOS/Pleyeces/Preferences/Controller/PreferencesViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PreferencesViewController.swift
+//  Pleyeces
+//
+//  Created by Kristiana on 18/01/2018.
+//  Copyright © 2018 Kristiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PreferencesViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var radiusText: UITextField!
+    @IBOutlet weak var radiusSlider: UISlider!
+    var types: Array<PoiType> = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return types.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "preferencesCell", for: indexPath) as! PreferencesCell
+        cell.poiNameLbl.text = types[indexPath.item].name
+        cell.poiBtn.backgroundColor = types[indexPath.item].color
+        cell.poiBtn.layer.cornerRadius = 0.5 * cell.poiBtn.bounds.size.width
+        cell.poiBtn.clipsToBounds = true
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width/3-9, height: collectionView.frame.size.width/3-9)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        PoiTypeFetcher.fetchAll { (poiTypes) in
+            self.types = poiTypes
+            self.collectionView.reloadData()
+        }
+        
+        // Do any additional setup after loading the view.
+        
+        radiusText.layer.cornerRadius = 11.0
+        radiusText.text = "\(Int(radiusSlider.value)) meters"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let value = Int(radiusSlider.value)
+        radiusText.text = "\(value) meters"
+    }
+
+}
