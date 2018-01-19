bb45b1bfc942f063ff412868d39e7f058bf4bb7a
krivite <kristiana735@gmail.com>
2018-01-19 14:25:51 +0100

    Replaced swiping... again. Managed by RootPageViewController now.

---
+++ iOS/Pleyeces/RootPageViewController.swift
@@ -0,0 +1,82 @@
+//
+//  RootPageViewController.swift
+//  Pleyeces
+//
+//  Created by Kristiana on 19/01/2018.
+//  Copyright © 2018 Kristiana. All rights reserved.
+//
+
+import UIKit
+
+class RootPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var viewControllerList:[UIViewController] = {
+        let sb = UIStoryboard(name:"Main", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "Explore")
+        let vc2 = sb.instantiateViewController(withIdentifier: "AR")
+        let vc3 = sb.instantiateViewController(withIdentifier: "Map")
+        
+        return [vc1, vc2, vc3]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: false, completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
+
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
+
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.white
+        appearance.currentPageIndicatorTintColor = UIColor.red
+        appearance.backgroundColor = UIColor.darkGray
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        setupPageControl()
+        return self.viewControllerList.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+
+}
